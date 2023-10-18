c6bb419ae94d5940fa19b12a9d177176dca7b03d
bread <chalida.bread@gmail.com>
2023-10-18 20:11:02 +0700

    fix: fix code and debugging

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,12 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+console.log(
+  lightBulbStatus === "On"
+    ? "Light bulb is on."
+    : lightBulbStatus === "Off"
+    ? "Light bulb is Off."
+    : lightBulbStatus === "Broken"
+    ? "Light bulb is Broken."
+    : "Please choose the correct input (On/Off)"
+);
